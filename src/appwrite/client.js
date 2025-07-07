360babb6fa4782d56eb5d3220f16f878ac87724d
Dev Sharma <devsharma1683@gmail.com>
2025-07-07 23:01:39 +0530

    fixed a bug in accessing the env vars

--- src/appwrite/client.js
+++ src/appwrite/client.js
@@ -1,8 +1,8 @@
 import { Client, Account, Databases, ID, Query } from 'appwrite'
 
 const client = new Client()
-    .setEndpoint(process.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(process.env.VITE_APPWRITE_PROJECT_ID)
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
 
 const account = new Account(client)
 const database = new Databases(client);
