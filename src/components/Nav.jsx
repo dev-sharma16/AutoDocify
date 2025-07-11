ec38921af08995b7684dbec98b35cb80988eae99
Dev Sharma <devsharma1683@gmail.com>
2025-07-11 14:52:39 +0530

    nav bar is funtional and redesigned to match the theme of the website and edit the logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -28,21 +28,41 @@ export default function Nav() {
         fetchUser();
     }, []);
     return (
-        <nav className='flex items-center px-15 py-3 justify-between bg-neutral-900 text-amber-50 fixed w-[100%] top-0'>
+        <nav className='flex items-center px-15 py-3 justify-between bg-neutral-900/30  w-[100%] fixed top-0 z-50 backdrop-blur-md border-b border-white/10 text-white shadow-sm'>
             <NavLink to='/'> 
                 <img 
-                    src="src/assets/Screenshot 2025-07-07 223358.png" 
+                    src="/src/assets/Screenshot 2025-07-07 223358-Picsart-BackgroundRemover.png" 
                     alt="logo" 
                     className='h-10'
                 />
             </NavLink>
-            <NavLink to='/'>Home</NavLink>
+            {/* <NavLink to='/'>Home</NavLink> */}
             {/* <NavLink to='/generate'>Make Doc</NavLink> */}
-            {user 
-                ? <button onClick={handleLogOut}>LogOut</button>
-                :<button onClick={handleClick}>Login</button>
-            }
             
+            {user 
+                ?<div> 
+                    <button 
+                        onClick={handleLogOut}
+                        className="bg-transparent border border-[#908CA1] text-[#908CA1] hover:bg-[#908CA1]/10 transition-colors px-6 py-3 rounded-lg font-medium shadow-md cursor-pointer"
+                    >
+                        LogOut
+                    </button>
+                </div>
+                :<div className="flex flex-col sm:flex-row gap-4">  
+                    <button 
+                        onClick={handleClick}
+                        className="bg-transparent border border-[#908CA1] text-[#908CA1] hover:bg-[#908CA1]/10 transition-colors px-6 py-3 rounded-lg font-medium shadow-md cursor-pointer"
+                    >
+                        Login
+                    </button>
+                    <button
+                            onClick={handleClick}
+                            className="bg-[#908CA0] hover:bg-[#605e6a] transition-colors text-black font-semibold px-6 py-3 rounded-lg shadow-lg"
+                        >
+                            Get Started
+                    </button>
+                </div>   
+            } 
         </nav>
     )
 }
